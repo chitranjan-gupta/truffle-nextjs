b683357544449f9f563f05b461e97aa2c62f480f
Chitranjan Gupta <chitranjankumargupta123@gmail.com>
2023-10-01 13:58:05 +0530

    Fixing the merge

--- client/next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  webpack: (config, { buildId, dev }) => {
-    // This allows the app to refer to files through our symlink
-    config.resolve.symlinks = false
-    return config
-  }
-}
